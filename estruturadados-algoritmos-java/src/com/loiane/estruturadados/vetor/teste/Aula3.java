041281dad49bbb61553d9e08dcb289baae91cabc
nilsantana <nilsantana@yahoo.com.br>
2018-11-16 13:04:56 -0200

    Melhorando o método adiciona

---
+++ estruturadados-algoritmos-java/src/com/loiane/estruturadados/vetor/teste/Aula3.java
@@ -0,0 +1,18 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula3 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(2);
+
+
+		vetor.adiciona("Curso");
+		vetor.adiciona("Estrutura de dados");
+		vetor.adiciona("elemento 3");
+	}  
+
+
+}
